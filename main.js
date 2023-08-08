97f3a714b0f3419144d8cd10f48248ad4cc5ce55
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2023-08-08 19:28:59 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 console.log('Podłączono main.js')
 
 //* Zmiena na dane o zarobkach
-const money = []
+var money = []
 
 //* Zmienne zawierające elementy z pliku HTML
 const SummaryTable = document.querySelectorAll('.LatesEarningList')[0].children[0].children[0]
@@ -23,13 +23,18 @@ const AllStatistics = document.querySelectorAll('.AllStatistics')[0]
 const MonthStatistics = document.querySelectorAll('.MonthStatistics')[0]
 const WeekStatistics = document.querySelectorAll('.WeekStatistics')[0]
 
+const DateSortAction = document.getElementById('SortByDateAction')
+
 var ActualMonthToCalculate = undefined
 var ActualCalculateWeekStart = undefined
 var ActualCalculateWeekEnd = undefined
 
+var DateSort = 1
+
 //* Dodawanie nasłuchiwaczy
 AddEarning.addEventListener('click',(e) => {
-    CreateRowTable(null, InputName.value, InputDate.value.split('-').reverse().join('.'), InputValue.value, null)
+    PushToMoney(null, InputName.value, InputDate.value.split('-').reverse().join('.'), InputValue.value, null)
+    sortTable()
 })
 MonthSelect.addEventListener('input', (e) => {
     let [ year , month ] = e.target.value.split('-')
@@ -42,6 +47,16 @@ WeekSelect.addEventListener('input', (e) => {
     ActualCalculateWeekStart = firstDayOfWeek
     ActualCalculateWeekEnd = lastDayOfWeek
     UpdateScreen()
+})
+DateSortAction.addEventListener('click',(e) => {
+    if (DateSort == 1) {
+        DateSort = 2 
+        DateSortAction.innerHTML = `Data <i class="bi bi-caret-up-fill"></i>`
+    } else if (DateSort == 2) {
+        DateSort = 1
+        DateSortAction.innerHTML = `Data <i class="bi bi-caret-down-fill"></i>`
+    }
+    sortTable()
 })
 
 //* Funkcja zwracająca aktualny czas
@@ -121,7 +136,9 @@ function StartUpSummary() {
     ActualCalculateWeekEnd = lastDayOfWeek
     // Czytanie danych z lokalnej bazy
     let readed = ReadFromLocalStorage('money')
-    readed.forEach((e) => {
+    let sorted = sortByDate(readed)
+    money = sorted
+    sorted.forEach((e) => {
         CreateRowTable(null, e.name, e.date, e.value, e.id)
     })
     // Wysyłanie wykonania innej funkcji
@@ -130,17 +147,29 @@ function StartUpSummary() {
 
 StartUpSummary()
 
-//* Funkcja aktualizująca ekran głowny
-function UpdateScreen() {
-    let AllSum = 0, MonthSum = 0, WeekSum = 0
+function CalculateAllSum() {
+    let AllSum = 0
     money.forEach((e) => {
-        // Ogólne
         AllSum += parseFloat(e.value)
-        // Miesiąc
-        if (parseInt(e.date.split('.')[1]) == ActualMonthToCalculate) {
+    })
+    return AllSum
+}
+
+function CalculateMonthSum(MonthNum) {
+    let MonthSum = 0
+    money.forEach((e) => {
+        if (parseInt(e.date.split('.')[1]) == MonthNum) {
             MonthSum += parseFloat(e.value)
         }
-        // Tydzień
+    })
+    // console.warn(MonthNum);
+    // console.warn(MonthSum);
+    return MonthSum
+}
+
+function CalculateWeekSum() {
+    let WeekSum = 0
+    money.forEach((e) => {
         let dateToCheck = new Date(String(e.date.split('.').reverse().join('-')))
         let startDate = new Date(`${ActualCalculateWeekStart.getFullYear()}-${ActualCalculateWeekStart.getMonth()+1}-${ActualCalculateWeekStart.getDate()}`)
         let endDate = new Date(`${ActualCalculateWeekEnd.getFullYear()}-${ActualCalculateWeekEnd.getMonth()+1}-${ActualCalculateWeekEnd.getDate()}`)
@@ -149,25 +178,46 @@ function UpdateScreen() {
             WeekSum += parseFloat(e.value)
         }
     })
+    return WeekSum
+}
+
+//* Funkcja aktualizująca ekran głowny
+function UpdateScreen() {
     // Ustawianie podsumowania ogólnego
-    AllStatistics.children[1].children[0].innerText = AllSum
+    AllStatistics.children[1].children[0].innerText =  CalculateAllSum()
     //  Ustawianie podsumowania miesięcznego
-    MonthStatistics.children[2].children[0].innerText = MonthSum
+    MonthStatistics.children[2].children[0].innerText = CalculateMonthSum(ActualMonthToCalculate)
+    let MonthDif = CalculateMonthDifrence().toString()
+    let MonthElementPercent = MonthStatistics.children[2].children[1]
+    MonthElementPercent.setAttribute('class','')
+    // console.warn(MonthDif);
+    if (MonthDif == '0') {
+        MonthElementPercent.classList.add('EarningSummary-Card-PercentNeutral')
+    } else if (MonthDif == '0.0') {
+        MonthElementPercent.classList.add('EarningSummary-Card-PercentGood')
+        MonthElementPercent.innerText = '0%'
+    } else {
+        MonthDif.charAt(0) === '-' ? MonthElementPercent.classList.add('EarningSummary-Card-PercentBad') : MonthElementPercent.classList.add('EarningSummary-Card-PercentGood');
+        // MonthElementPercent.innerText = `${MonthDif.slice(1).charAt(0) === '.' ? '0'+MonthDif.slice(1) : MonthDif.slice(0)}%`
+        if (MonthDif > 0) {
+            MonthElementPercent.innerText = `${MonthDif}%`
+        } else {
+            MonthElementPercent.innerText = `${MonthDif.slice(1)}%`
+        }
+    }
     //  Ustawianie podsumowania tygodniowego
-    WeekStatistics.children[2].children[0].innerText = WeekSum
+    WeekStatistics.children[2].children[0].innerText = CalculateWeekSum()
+
+    
 }
 
 //* Funkcja tworząca elemnty do tabeli z ostatnimi operacjami
 //* Wywołanie funkcji zapisującej
 function CreateRowTable(parentElement, name, date, value, CustomId) {
-    let gate
     parentElement = parentElement === null ? SummaryTable : parentElement;
     name = name == '' || name == null ? "Brak Nazwy" : name
     date = date == null ? InputDate.value.split('-').reverse().join('.') : date
     value = value == null || value == '' ? "0" : value
-    if(CustomId == null || CustomId == '') {
-        gate = 1
-    }
     CustomId = CustomId == null || CustomId == '' ? GetId() : CustomId
     let row = document.createElement('tr')
     row.classList.add('RowTable')
@@ -182,16 +232,44 @@ function CreateRowTable(parentElement, name, date, value, CustomId) {
         <td><button onclick='RemoveRowAndData(this)' class="LatesEarningList-RemoveRow"><i class="bi bi-trash-fill"></i></button></td>
     `
     parentElement.appendChild(row)
+}
 
+function PushToMoney(parentElement, name, date, value, CustomId) {
+    parentElement = parentElement === null ? SummaryTable : parentElement;
+    name = name == '' || name == null ? "Brak Nazwy" : name
+    date = date == null ? InputDate.value.split('-').reverse().join('.') : date
+    value = value == null || value == '' ? "0" : value
+    CustomId = CustomId == null || CustomId == '' ? GetId() : CustomId
     money.push({
         id: CustomId,
         name: String(name),
         date: String(date),
+        // date: new Date(date.split('.').reverse().join('-')),
         value: parseFloat(value)
     })
-    if(gate == 1) {
-        SaveToLocalStorage('money', money)
+    SaveToLocalStorage('money', money)
+}
+
+function sortTable() {
+    document.querySelectorAll('.RowTable').forEach((e) => {e.remove()})
+
+    let readed = ReadFromLocalStorage('money')
+    var sorted = sortByDate(readed)
+    money = sorted
+
+    if (DateSort == 1) {
+        //? Sortowanie według daty od najstarszej do najnowszej
+        sorted.forEach((e) => {
+            CreateRowTable(null, e.name, e.date, e.value, e.id)
+        })
+    } else if (DateSort == 2) {
+        //? Sortowanie według daty od najnowszej do najstarszej
+        for (let i = sorted.length; i > 0; i--) {
+            let e = sorted[i-1]
+            CreateRowTable(null, e.name, e.date, e.value, e.id)
+        }
     }
+
 }
 
 //* Funkcja usuwająca wiersz wraz z danymi z bazy danych
@@ -236,3 +314,32 @@ function RemoveFromLocalStorage(name) {
     UpdateScreen()
 }
 
+function CalculateMonthDifrence() {
+    let actualMonthMoney = CalculateMonthSum(ActualMonthToCalculate)
+    let prieviusMonthMoney
+    if (ActualMonthToCalculate-1 < 1) {
+        prieviusMonthMoney = CalculateMonthSum(12)
+    } else {
+        prieviusMonthMoney = CalculateMonthSum(ActualMonthToCalculate-1)
+    }
+    // console.warn('Calculate Month');
+    // console.log(actualMonthMoney);
+    // console.log(prieviusMonthMoney);
+
+    let percent = (actualMonthMoney - prieviusMonthMoney)/prieviusMonthMoney * 100
+    // let operator = prieviusMonthMoney < actualMonthMoney ? '+' : '-' 
+    // console.warn(`${percent.toFixed(1)}%`);
+    if (percent.toFixed(1) == 'Infinity' || percent.toFixed(1) == 'NaN') {
+        return 0
+    } else {
+        return percent.toFixed(1)
+    }
+} 
+
+function sortByDate(data) {
+    function parseDate(dateString) {
+        const parts = dateString.split('.');
+        return new Date(parts[2], parts[1] - 1, parts[0]);
+    }
+    return data.slice().sort((a, b) => parseDate(a.date) - parseDate(b.date));
+}
